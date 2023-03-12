e3d711b8dc6980683c671e71cb377eca4ebda7c0
Marcin Florek <florekdeveloper@gmail.com>
2023-03-12 16:43:54 +0100

    web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import AboutAuthor from "./features/AboutAuthor";
+import { Container } from "./features/Container";
+import SkillsList from "./features/SkillsList";
+import SwitchButton from "./features/SwitchButton";
+import { theme } from "./theme.js";
+import { futureSkills, skills } from "./features/skills";
+import Portfolio from "./features/Portfolio/index"
+import { useDispatch } from "react-redux";
+import { fetchGithubRepos } from "./features/repos/reposSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container onLoad={() => dispatch(fetchGithubRepos())}>
+        <SwitchButton />
+        <AboutAuthor />
+        <SkillsList header={"My skillset includes 🛠️"} skills={skills} />
+        <SkillsList
+          header={"What I want to learn next 🚀"}
+          skills={futureSkills}
+        />
+        <Portfolio />
+      </Container>
+    </ThemeProvider>
   );
 }
 
